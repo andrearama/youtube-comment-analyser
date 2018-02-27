580b870088439973ab04d6d050069bc404006e42
Andrea Ramazzina <andrea_ramazzina@hotmail.it>
2018-02-27 13:32:24 +0100

    Data import

---
+++ datahandler.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 27 13:24:35 2018
+
+@author: Andrea Ramazzina
+"""
+import csv
+
+
+def import_comments(file_name): 
+    """
+    Extract a list of the comments from the csv data file.
+    """    
+    
+    if 'csv' not in file_name:
+        file_name = file_name + '.csv'
+    
+    failed_retrivials_number = 0
+    comments = [];
+    with open('data.csv',encoding="utf8") as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+        for row in csvReader:
+            for count,element in enumerate(row):
+                if 'ago' in element:
+                    try:
+                        comments.append(row[count+2])
+                    except IndexError:                
+                        failed_retrivials_number = failed_retrivials_number+1
+                    break
+    
+    print('Total number of comments unsuccessfully extracted:'+ str(len(comments)-failed_retrivials_number))   
+    print('Total number of comments unsuccessfully extracted:'+ str(failed_retrivials_number))        
+
+    return     
---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 27 12:50:48 2018
+
+@author: Andrea Ramazzina
+"""
+
+from datahandler import *
+
+
+
+
+
+comments = import_comments('data.csv')             
