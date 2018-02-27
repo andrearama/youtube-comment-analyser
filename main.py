b8c2660240630e2b0da9366797e1496adc7ac5f0
Andrea Ramazzina <andrea_ramazzina@hotmail.it>
2018-02-27 14:19:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ Created on Tue Feb 27 12:50:48 2018
 
 @author: Andrea Ramazzina
 """
-
 from datahandler import *
-
-
-
-
+from statistical_comments_analysis import *
 
 comments = import_comments('data.csv')             
+
+lexical_diversity = compute_lexical_diversity(comments)
+print(lexical_diversity)
