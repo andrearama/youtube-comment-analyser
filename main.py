df9fe40adeca2082efd561fecdd7137ce3079a71
Andrea Ramazzina <andrea_ramazzina@hotmail.it>
2018-02-28 14:41:12 +0100

    Add first sentiment clustering

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ Created on Tue Feb 27 12:50:48 2018
 """
 from datahandler import *
 from statistical_comments_analysis import *
+from sentiment_analysis import *
 
 comments = import_comments('data.csv')             
 
 lexical_diversity = compute_lexical_diversity(comments)
 print(lexical_diversity)
+
+positive,negative,neutral = determine_sentiment(comments)
