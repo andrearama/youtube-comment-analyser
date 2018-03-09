2331a600763eec199b1b8b68f83b224e5860d524
andrama <andrea_ramazzina@hotmail.it>
2018-03-09 13:06:39 +0100

    tested new functions

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ Created on Tue Feb 27 12:50:48 2018
 from datahandler import *
 from statistical_comments_analysis import *
 from sentiment_analysis import *
+from NLP import *
 
-comments = import_comments('data.csv')             
+#comments = import_comments('data.csv')             
 
-lexical_diversity = compute_lexical_diversity(comments)
-print(lexical_diversity)
+#lexical_diversity = compute_lexical_diversity(comments)
+#print(lexical_diversity)
 
-positive,negative,neutral = determine_sentiment(comments)
+#positive,negative,neutral = determine_sentiment(comments[0:30])
+
+sentence = 'Hello, my name is Andrea. Actually, I have two names: Andrea and Ren Nan.'
+a =preprocess_sentence(sentence, output_type = 'TextBlob')
+
+print(a)
