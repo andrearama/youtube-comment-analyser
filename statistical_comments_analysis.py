99f6a4f21f18976f045b5c9605198ce39166c376
andrama <andrea_ramazzina@hotmail.it>
2018-03-09 13:05:52 +0100

    Fixed some bugs

--- statistical_comments_analysis.py
+++ statistical_comments_analysis.py
@@ -7,9 +7,7 @@ Created on Tue Feb 27 14:14:26 2018
 
 from nltk.tokenize import TweetTokenizer
 from sklearn.feature_extraction.text import CountVectorizer
-from nltk.corpus import stopwords
-import TextBlob
-import textblob
+
 
 def compute_lexical_diversity(comments):
     """
@@ -26,28 +24,7 @@ def compute_lexical_diversity(comments):
     return lexical_diversity
 
 
-def preprocess_sentence(sentence, output_type = 'TextBlob'):
-    """
-    Put in lowecase filter out stopwords and stem a single sentence
-    Input: String or TextBlob object 
-    """
-    if type(sentence) == str:
-        sentence = TextBlob(sentence)
-    elif type(sentence) != textblob.blob.TextBlob:
-         raise ValueError('Input is neither a string or a TextBlob object')
-           
-    sentence = sentence.lower()
-    word_list = sentence.words
-    filtered_words = (word.stem() for word in word_list if word not in stopwords.words('english'))
-    
-    if output_type == 'TextBlob':
-       return TextBlob(' '.join(filtered_words))
-    elif output_type == 'tuple':
-       return filtered_words
-    elif output_type == 'string':
-        return ' '.join(filtered_words)
-    else:
-        raise ValueError('Output tpye not understood')
+
 
 def bag_of_words_sentence(sentence):
     """
