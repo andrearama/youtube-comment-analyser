44a2d4ff02fa93509a39b8900b807e6b8f59dcdd
andrama <andrea_ramazzina@hotmail.it>
2018-03-11 21:24:32 +0100

    specify bag_of_words outputs

--- statistical_comments_analysis.py
+++ statistical_comments_analysis.py
@@ -2,12 +2,12 @@
 """
 Created on Tue Feb 27 14:14:26 2018
 
-@author: andre
+@author: Andrea Ramazzina
 """
 
 from nltk.tokenize import TweetTokenizer
 from sklearn.feature_extraction.text import CountVectorizer
-
+from sklearn.preprocessing import normalize
 
 def compute_lexical_diversity(comments):
     """
@@ -28,11 +28,11 @@ def compute_lexical_diversity(comments):
 
 def bag_of_words_sentence(sentence):
     """
-    Return bag of words extracted from the sentence
+    Return bag of words extracted from the sentence (word count)
     Input: TextBlob object
-    Output: defaultdict
+    Output: defaultdict object
     """
-    
+    print("Attention: the values are the words count, not frequency")
     bag = sentence.word_counts
     return bag
 
@@ -40,11 +40,12 @@ def bag_of_words_corpus(corpus):
     """
     Return bag of words extracted from the sentence
     Input: List of sentences
-    Output: List of lists containing the number of times for each word
+    Output: List of lists containing the frequency of each word
     """    
     vectorizer = CountVectorizer()
     features = vectorizer.fit_transform(corpus).todense() 
-    return features
+    features_norm = normalize(features, axis=1, norm='l1')
+    return features_norm
    
     #from sklearn.metrics.pairwise import euclidean_distances
     #    print(euclidean_distances(features[0],f))
